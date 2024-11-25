5737232df6580629706f32b46094dbfaf439009b
helloAmulya <internatomix@gmail.com>
2024-11-25 10:34:36 +0530

    final coede files

--- scr.js
+++ scr.js
@@ -1,3 +1,16 @@
+const menuButton = document.querySelector('.menu-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileLinks = document.querySelectorAll('.mobile-nav-item');
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open'); 
+});
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('translate-x-[100%]');
+    });
+});
+
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -6,7 +19,6 @@ const observer = new IntersectionObserver((entries, observer) => {
         }
     });
 }, { threshold: 0.5 });
-
 
 const sections = document.querySelectorAll('.section-to-animate');
 sections.forEach(section => {
